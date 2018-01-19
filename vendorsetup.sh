c19da0ce5cd3ec410805e9040fbe4e3eae1df395
KaoTD <kaotd99@gmail.com>
2018-01-19 05:46:43 +0000

    rebrand hexagon

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_ef60-user
-add_lunch_combo lineage_ef60-userdebug
-add_lunch_combo lineage_ef60-eng
+add_lunch_combo hexagon_ef60-user
+add_lunch_combo hexagon_ef60-userdebug
+add_lunch_combo hexagon_ef60-eng
